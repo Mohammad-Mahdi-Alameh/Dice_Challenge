ea97d470db28b6628853f891f05c805d3488a4c4
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-23 10:50:16 +0300

    Created function getRandom to generate random index between 0-6

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
 window.onload = chooseRandomPictures;
+
 img1=document.getElementById("img1");
+
 img2=document.getElementById("img2");
+
+
+function getRandom(min, max) {    //function to give random integer between 2 values; min and max
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 var myPix = ["../images/dice1.png","../images/dice2.png","../images/dice3.png","../images/dice4.png","../images/dice5.png","../images/dice6.png"];
 
+function chooseRandomPictures(){
+
+
+}
