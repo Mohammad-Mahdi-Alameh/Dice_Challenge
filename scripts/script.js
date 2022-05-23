fbc4ff14479f16c192c781108f5cf30b5c8f91fe
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-23 11:21:04 +0300

    Adding code to display the result

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 window.onload = chooseRandomPictures;
+displayWinner();
 
 img1=document.getElementById("img1");
 
 img2=document.getElementById("img2");
+
+header=document.getElementById("header");
 
 
 function getRandom(min, max) {    //function to give random integer between 2 values; min and max
@@ -15,17 +18,27 @@ function getRandom(min, max) {    //function to give random integer between 2 va
 
 var dices = ["../images/dice1.png","../images/dice2.png","../images/dice3.png","../images/dice4.png","../images/dice5.png","../images/dice6.png"];
 
-let random_index;
+var random_index_img1 , random_index_img2 ;
 
 function chooseRandomPictures(){
 
-random_index=getRandom(0,5);
+random_index_img1=getRandom(0,5);
 
-img1.src=dices[random_index];
+img1.src=dices[random_index_img1];
 
-random_index=getRandom(0,5);
+random_index_img2=getRandom(0,5);
 
-img2.src=dices[random_index];
+img2.src=dices[random_index_img2];
 
+    if(random_index_img1 > random_index_img2)
+
+        header.innerHTML=" Player 1 Wins!";
+
+    else if(random_index_img1 < random_index_img2)
+
+        header.innerHTML=" Player 2 Wins!";
+
+    else header.innerHTML="Draw!";
 
 }
+
