1aa235bd319f0ccb06b486cc92b5610f0a6bcdee
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-23 10:48:00 +0300

    Loaded the images into an array

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+window.onload = chooseRandomPictures;
+img1=document.getElementById("img1");
+img2=document.getElementById("img2");
+
+var myPix = ["../images/dice1.png","../images/dice2.png","../images/dice3.png","../images/dice4.png","../images/dice5.png","../images/dice6.png"];
+
